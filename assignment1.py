990b5e64699291eccb1b386586d2e349653519ff
Jaehyun05 <jtic0524@gmail.com>
2025-10-29 22:20:18 +0900

    1주차 과제

--- assignment1.py
+++ assignment1.py
@@ -1,296 +1,77 @@
-class Matrix:
-    def __init__(self, elements):
-        """행렬
+def get_file():
+    """로그를 작성하려고 시도한 위치를 찾는 함수
+
+    **Logger.log에서 호출되어야만 정상적으로 작동합니다**
+    """
+    import inspect
+    import os
+
+    frame = inspect.stack()[2]
+    return os.path.basename(frame.filename)
+
+
+class Logger:
+    """싱글톤 패턴을 적용한 Logger
+
+    실제 실험 코드에서 Logger를 전역 변수로 생성하는 것은 권장되지 않습니다.
+    그래서 로그를 남기는 것이 필요한 경우에는 함수 내에서 로거를 가져와야 합니다.
+    한 편, 로거를 호출할 때마다 새로운 인스턴스가 생성되면 이것이 불가능하겠죠?
+
+    싱글톤 패턴을 통해 Logger를 여러번 호출하더라도 한 번만 불러와지도록 설계해봅시다!
+    `_path`로 지정된 위치에 파일을 만들어서 로그를 차례로 작성하도록 해주세요.
+
+    주의!
+    - 기존에 로그 파일이 있었다면, 파일이 덮어씌워지도록 설계되어야 합니다.
+    """
+
+    _instance = None
+    _path = "test.log"
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls.single = []
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
+    def __init__(self): ...
+
+    def log(self, message):
+        """로그 작성
+
+        로그는 반드시, [시간 | 호출 위치] > 메세지의 형식으로 작성해주세요.
+
+        e.g. [2025-10-03 21:45:23 | main.py] > Logger가 초기화되었습니다.
+
+        현재 호출 위치는 일반적인 방법으로 찾기 힘들어서, get_file 함수를 만들었습니다. 사용해주세요!
 
         Args:
-            elements: 크기가 `n`인 리스트 `m`개로 이루어진 리스트
+            message: 새로운 메세지
         """
-        if isinstance(elements, list) :
-            first_row = len(elements[0])
-            for row in elements :
-                if isinstance(row, list) :
-                    if len(row) != first_row :
-                        raise ValueError("행의 길이가 같아야 합니다!")
-                
-                else :
-                    raise ValueError("행은 리스트로 입력")
-        else : 
-            raise ValueError("행렬은 이중리스트로 입력")        
-        self.elements = elements
-        self.shape = (len(elements), len(elements[0]) if elements else 0)
+        import time
 
-    def __str__(self):
-        """행렬의 문자열 형식
-        
-        Examples:
+        self.single.append(self._instance)
 
-            >>> print(Matrix([[1, 2], [3, 4], [5, 6]]))
-            [[1, 2], [3, 4], [5, 6]]
-        """
-        return str(self.elements)
+        if len(self.single) >= 2:
+            self._initialized_flag = False  # 이미 초기화 됨
 
-    def __repr__(self):
-        """행렬의 선언 문자열 형식
-        
-        Examples:
+        else:
+            self._initialized_flag = True  # 크기가 1이면 새로 초기화 된 것
 
-            >>> Matrix([[1, 2], [3, 4], [5, 6]])
-            Matrix([[1, 2], [3, 4], [5, 6]])
-        """
-        return f"Matrix{(self.elements)}"
+        if self._initialized_flag:
+            now_file_path = get_file()
+            origin_time = time.time()
+            struct_time = time.localtime(origin_time)
+            now = time.strftime("%Y-%m-%d %H:%M:%S", struct_time)
+            with open(self._path, "w", encoding="utf-8") as f:
+                f.write(f"[{now} | {now_file_path}] > {message}\n")
 
-    def __add__(self, other):
-        """행렬의 덧셈
+        elif not self._initialized_flag:
+            now_file_path = get_file()
+            origin_time = time.time()
+            struct_time = time.localtime(origin_time)
+            now = time.strftime("%Y-%m-%d %H:%M:%S", struct_time)
+            with open(self._path, "a", encoding="utf-8") as f:
+                f.write(f"[{now} | {now_file_path}] > {message}\n")
 
-        행렬이 아닌 경우 예외 처리도!
-
-        Args:
-            other: 더할 m×n 행렬.
-        """
-        if not (isinstance(other, Matrix) and other.shape == self.shape) :
-            raise ValueError("행렬의 크기가 같아야합니다!")
-        
-        results = []
-        for i in range(self.shape[0]) :
-            row = []
-            for j in range(self.shape[0]) :
-                row.append(self.elements[i][j] + other.elements[i][j])
-            results.append(row)
-
-        return Matrix(results)
-    def __sub__(self, other):
-        """행렬의 뺄셈
-
-        Args:
-            other: 뺄 m×n 행렬.
-        """
-        if not (isinstance(other, Matrix) and other.shape == self.shape) :
-            raise ValueError("행렬의 크기가 같아야합니다!")
-        
-        results = []
-        for i in range(self.shape[0]) :
-            row = []
-            for j in range(self.shape[1]) :
-                row.append(self.elements[i][j] - other.elements[i][j])
-            results.append(row)
-        
-        return Matrix(results)
-
-
-    def __iadd__(self, other):
-        """in-place 덧셈
-
-        행렬을 새로 생성하지 않고 원소의 값을 수정! 행렬이 아닌 경우 예외처리도!
-
-        Args:
-            other: 더할 m×n 행렬.
-        """
-        if not (isinstance(other, Matrix) and other.shape == self.shape) :
-            raise ValueError("행렬의 크기가 같아야합니다!")
-        
-        for i in range(self.shape[0]):
-            for j in range(self.shape[1]):
-                self.elements[i][j] = self.elements[i][j] + other.elements[i][j]
-
-        return Matrix(self.elements)
-    
-    # __sub__는 __neg__를 응용하시면 좋을 것 같습니다!
-
-    def __neg__(self):
-        """음연산
-
-        행렬의 모든 원소의 부호를 바꾸는 연산 -A를 정의합니다.
-        """
-        results = []
-        for i in range(self.shape[0]):
-            row = []
-            for j in range(self.shape[1]):
-                row.append(self.elements[i][j] * (-1))
-            results.append(row)
-        
-        return Matrix(results)
-
-    def __mul__(self, other):
-        """행렬곱
-
-        행렬이 아닌 경우 예외처리도!
-
-        Args:
-            other: 곱할 n×k 행렬.
-        """
-        if not (isinstance(other, Matrix) and other.shape[0] == self.shape[1]) :
-            raise ValueError("앞 행렬의 열 개수와 뒷 행렬의 행 개수가 같아야 합니다!")
-        
-        results = []
-        for m in range(self.shape[0]):
-            row = []
-            for n in range(other.shape[1]):
-                atom_sum = 0
-                for k in range(self.shape[1]):
-                    atom_sum += self.elements[m][k] * other.elements[k][n]
-                row.append(atom_sum)
-            results.append(row)
-
-        return Matrix(results)            
-
-
-    def __rmul__(self, other):
-        """스칼라배
-
-        스칼라(`int`나 `float`)이 아닌 경우 예외처리도!
-
-        Args:
-            other: 곱할 스칼라.
-        """
-        if not (isinstance(other, int) or isinstance(other, float)) :  
-            raise ValueError("실수를 입력해주세요!")
-        
-        results = []
-        for i in range(self.shape[0]):
-            row = []
-            for j in range(self.shape[1]):
-                row.append(self.elements[i][j] * other)
-            results.append(row)
-        
-        return Matrix(results)
-
-    def __imul__(self, other):
-        """in-place 곱셈
-
-        행렬을 새로 생성하지 않고 내부 값을 수정! 행렬이나 스칼라가 아닌 경우 예외처리도!
-
-        Args:
-            other: 곱할 n×k 행렬 또는 스칼라.
-        """
-        if not (isinstance(other, int) or isinstance(other, float) or (isinstance(other, Matrix) and other.shape[0] == self.shape[1])) :
-            raise ValueError("실수를 입력해주세요!")
-
-        if isinstance(other, int) or isinstance(other, float) :
-            for i in range(self.shape[0]) :
-                for j in range(self.shape[1]) :
-                    self.elements[i][j] = self.elements[i][j] * other
-            return self.elements
-        
-        elif isinstance(other, Matrix) and other.shape[0] == self.shape[1] :
-            for m in range(self.shape[0]):
-                for _ in range(self.shape[1]):
-                    row = []
-                    for k in range(other.shape[1]):
-                        atom_sum = 0
-                        for n in range(other.shape[0]) :
-                            atom_sum += self.elements[m][n] * other.elements[n][k]
-                        row.append(atom_sum)
-                self.elements[m] = row
-            return self.elements
-        
-        else :
-            raise ValueError("크기가 맞는 행렬이나 실수를 입력해주세요!")
-            
-
-
-    def __contains__(self, other):
-        """원소 포함 확인
-
-        Args:
-            other: 행렬에 속해 있는 지 확인할 스칼라.
-        """
-        for i in range(self.shape[0]) :
-            for j in range(self.shape[1]) :
-                if self.elements[i][j] == other :
-                    return True
-                
-        return False
-
-
-    def __len__(self):
-        """행렬의 차원
-
-        행렬의 차원, 즉 행의 개수(m)을 반환합니다.
-        """
-        return len(self.elements)
-
-    def __iter__(self):
-        """행렬의 반복자
-
-        차례대로 각 행을 리스트 형태로 반환합니다. `yield`와 `Generator`를 사용해 구현하세요!
-
-        Examples:
-
-            >>> for col in Matrix([[1, 2], [3, 4], [5, 6]]):
-            ... print(col)
-            [1, 2]
-            [3, 4]
-            [5, 6]
-        """
-        for i in self.elements :
-            yield i
-
-    def row(self, i):
-        """`i`번째 행을 리스트 형태로 반환합니다. 0부터!"""
-        return self.elements[i]
-
-    def col(self, j):
-        """`j`번째 열을 리스트 형태로 반환합니다. 0부터!"""
-        results = []
-        for row in self.elements :
-            results.append(row[j])
-
-        return results
-        
-    def __getitem__(self, key):
-        """행렬의 아이템
-
-        여러 개를 적으면 `tuple` 형식으로 키가 들어옵니다(예를 들어, matrix[2, 3]에서 key=(2, 3)).
-        `int`가 하나 들어온 경우 해당 번째의 행을 리스트 형태로 반환합니다.
-        `int`가 두 개 들어온 경우 해당 위치의 원소를 반환합니다.
-
-        마찬가지로 0부터임을 잘 생각해주세요!
-
-        Args:
-            *key: 얻을 원소 또는 행의 위치
-        """
-        if isinstance(key, int) :
-            return self.elements[key]
-        
-        elif isinstance(key, tuple) and len(key) == 2 :
-            return self.elements[key[0]][key[1]]
-        
-        else :
-            raise ValueError("길이가 1 또는 2인 key로 입력해주세요!")
-
-
-
-    def __eq__(self, other):
-        """두 행렬이 같은 지 확인합니다.
-
-        행렬이 같다는 것은 같은 위치의 원소가 모두 같다는 것을 의미합니다!
-
-        Args:
-            other: 행렬
-        """
-        if not (isinstance(other, Matrix) and other.shape == self.shape) :
-            raise ValueError("행렬의 크기가 같아야합니다!")
-        
-        for i in range(self.shape[0]) :
-            for j in range(self.shape[1]) :
-                if not self.elements[i][j] == other.elements[i][j] :
-                    return False
-                
-        return True
-
-
-# 테스트 코드
-
-A = Matrix([[1, 2], [3, 4]])
-B = Matrix([[5, 6], [7, 8]])
-
-print(A + B)                  # [[6, 8], [10, 12]]
-print(A - B)                    # [[-4, -4], [-4, -4]]
-print(A * B)                    # [[19, 22], [43, 50]]
-print(3 * A)                    # [[3, 6], [9, 12]]
-print(2 in A)
-print(A.row(0))
-print(B.col(0))
-print(A[1, 0])                  # 3
-for row in A:
-    print(row, end=", ")        # [1, 2], [3, 4]
+        else:
+            raise ValueError("ERROR!")
--- website/docusaurus.config.mts
+++ website/docusaurus.config.mts
@@ -3,6 +3,8 @@ import { themes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import tailwindPlugin from './plugins/tailwindPlugin';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
   title: '도란보리',
@@ -33,6 +35,8 @@ const config: Config = {
           path: 'docs',
           sidebarPath: './sidebars.ts',
           editUrl: 'https://github.com/doranbori/doranbori-hub/tree/main/website/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex]
         },
         blog: {
           showReadingTime: true,
@@ -135,6 +139,15 @@ const config: Config = {
       darkTheme: themes.gruvboxMaterialDark,
     },
   } satisfies Preset.ThemeConfig,
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ]
 };
 
 export default config;
